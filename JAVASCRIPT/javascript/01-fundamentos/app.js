64061a9db4d31c110f34fdb7dd3198bea2fb6309
sergiioog <sergiogargor9@gmail.com>
2025-07-04 23:01:46 +0200

    feat(js-rxjs): js and rxjs courses

---
+++ JAVASCRIPT/javascript/01-fundamentos/app.js
@@ -0,0 +1,28 @@
+console.log("Adios sergio");
+
+// let a = 10; 
+// var b = 10;
+// const c = 10; //Valor de memoria cuyo resultado no va a poder cambiar
+// // c = 20; No se puede asignar un valor a una constante
+
+// b = 20;
+// a = 30;
+
+
+//Polyfill -> codigo que provee funcionalidades nuevas de js en versiones antiguas
+
+let a = 10;
+let b = 10;
+let c = 'Hola ';
+let d = 'Spiderman';
+let e = 10;
+let x = a + b;
+
+// console.log( x );
+// console.warn(x);
+// console.error(x);
+// console.info(x);
+// console.log('%c Mis variables', 'color:blue; font-weight:bold')
+const saludo = c + d
+// console.table({a,b,c,d,e,x})
+console.log(">>> saludo ", saludo)
